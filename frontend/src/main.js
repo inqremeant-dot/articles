87cca7d05937d64ddbff33720875b039193c14ef
inqremeant <imzhizhin@gmail.com>
2026-08-25 19:26:00 +0300

    fix(frontend): add @mdi/font import and configure Vuetify icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,14 +5,22 @@ import store from './store';
 
 // Vuetify
 import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
-import '@mdi/font/css/materialdesignicons.css';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 });
 
 const app = createApp(App);
